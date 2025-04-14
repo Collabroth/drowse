9ce7ae4bd96bc6f417c98a699dac9ec265391a04
Jayman Rana <jayman.code@gmail.com>
2025-04-14 00:58:26 -0400

    refactor theme and add swipeable tabs

--- app/src/main/java/com/codebroth/rewake/RewakeApp.kt
+++ app/src/main/java/com/codebroth/rewake/RewakeApp.kt
@@ -2,24 +2,32 @@ package com.codebroth.rewake
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.codebroth.rewake.feature.calculator.CalculatorScreen
 import com.codebroth.rewake.ui.components.RewakeAppBar
+import com.codebroth.rewake.ui.theme.RewakeTheme
 
 @Composable
 fun RewakeApp() {
-    Scaffold(
-        topBar ={
+    Scaffold (
+        topBar = {
             RewakeAppBar()
         }
-    ) {
+    ) { innerPadding ->
         CalculatorScreen(
             modifier = Modifier.fillMaxSize()
-                .padding(it)
+                .padding(innerPadding)
         )
+    }
+}
+
+@Preview
+@Composable
+fun RewakeAppPreview() {
+    RewakeTheme {
+        RewakeApp()
     }
 }
--- app/src/main/java/com/codebroth/rewake/feature/calculator/CalculatorScreen.kt
+++ app/src/main/java/com/codebroth/rewake/feature/calculator/CalculatorScreen.kt
@@ -1,23 +1,36 @@
 package com.codebroth.rewake.feature.calculator
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BrightnessHigh
 import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.outlined.BrightnessHigh
 import androidx.compose.material.icons.outlined.DarkMode
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalculatorScreen(modifier: Modifier = Modifier) {
 
@@ -38,6 +51,16 @@ fun CalculatorScreen(modifier: Modifier = Modifier) {
         mutableIntStateOf(0)
     }
 
+    val pagerState = rememberPagerState {
+        calculatorTabItems.size
+    }
+
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(pagerState.targetPage) {
+        selectedTabIndex = pagerState.targetPage
+    }
+
     Column(
         modifier = modifier
     ) {
@@ -46,6 +69,9 @@ fun CalculatorScreen(modifier: Modifier = Modifier) {
                 Tab(
                     selected = index == selectedTabIndex,
                     onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
                         selectedTabIndex = index
                     },
                     text = {
@@ -64,6 +90,18 @@ fun CalculatorScreen(modifier: Modifier = Modifier) {
                 )
             }
         }
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxWidth()
+                .weight(1f)
+        ) { index ->
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = calculatorTabItems[index].title)
+            }
+        }
     }
 }
 
@@ -72,3 +110,15 @@ data class CalculatorTabItem(
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
 )
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun CalculatorScreenPreview() {
+    Surface {
+        CalculatorScreen(
+            modifier = Modifier
+                .fillMaxSize()
+        )
+    }
+}
