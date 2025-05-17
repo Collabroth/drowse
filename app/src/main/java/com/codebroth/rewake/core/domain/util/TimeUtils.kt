d5c69bfa61b7e78625845e7e590a9c3bbb889e76
Jayman Rana <jayman.code@gmail.com>
2025-05-17 00:16:46 -0400

    redesign details dialog for reminder

--- app/src/main/java/com/codebroth/rewake/core/domain/util/TimeUtils.kt
+++ app/src/main/java/com/codebroth/rewake/core/domain/util/TimeUtils.kt
@@ -2,6 +2,7 @@ package com.codebroth.rewake.core.domain.util
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TimePickerState
+import java.time.DayOfWeek
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
@@ -11,6 +12,9 @@ import java.time.format.DateTimeFormatter
 object TimeUtils {
 
     private val formatter = DateTimeFormatter.ofPattern("h:mm a")
+
+    private val WEEKEND = setOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
+    private val WEEKDAYS = DayOfWeek.entries.toSet() - WEEKEND
 
     /**
      * Formats a LocalTime object to a string in the format "h:mm a".
@@ -28,5 +32,23 @@ object TimeUtils {
             timePickerState.hour,
             timePickerState.minute
         )
+    }
+
+    fun summarizeSelectedDaysOfWeek(days: Set<DayOfWeek>): String = when {
+        days.isEmpty() -> "None"
+        days.size == 7 -> "Everyday"
+        days == WEEKDAYS -> "Weekdays"
+        days == WEEKEND -> "Weekend"
+        else -> {
+            days
+                .sortedBy { it.value }
+                .joinToString { day ->
+                    day
+                        .name
+                        .take(3)
+                        .lowercase()
+                        .replaceFirstChar { it.uppercase() }
+                }
+        }
     }
 }
